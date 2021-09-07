ee00cea5c093404047713166f185d59e85d9810b
yashk1873 <56143267+yashk1873@users.noreply.github.com>
2021-09-07 22:29:09 +0530

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,20 @@
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+
+for (var i = 0; i < names.length; i++) {
+
+  
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+ 
+  if (names[i].charAt(0) === 'j' || names[i].charAt(0) === 'J' ) {
+    console.log("Goodbye "+ names[i] + " " + name);
+  } else {
+    console.log("hello "+ names[i] + " " + name);
+  }
+}
+
+})();
 
